e8c9da593870189bcd053c475ecae8f1108f4930
Ukureii <98332714+Ukureii@users.noreply.github.com>
2024-05-18 19:05:00 +0200

    changing icon

--- app/components/NavBar.js
+++ app/components/NavBar.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   Box,
   Button,
@@ -6,43 +7,51 @@ import {
 } from '@chakra-ui/react';
 
 const navLinks = [
-  { name: 'Accueil', path: '#' },
-  { name: 'Projets', path: '#projets' },
-  { name: 'Compétences', path: '#competences' },
-  { name: 'Contact', path: '#contact' }
+  { name: 'Accueil', path: 'accueil' },
+  { name: 'Projets', path: 'projets' },
+  { name: 'Compétences', path: 'competences' },
+  { name: 'Contact', path: 'contact' }
 ];
 
 const NavLink = (props) => {
   const { children, path } = props;
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const element = document.getElementById(path);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <Button
-      as="a"
-      _hover={{ bg: "rgba(34, 139, 230, 0.05)"}}
-      _active={{ bg: "rgba(34, 139, 230, 0.1)"}}
-      rounded={'full'}
-      fontWeight={'normal'}
-      variant={'ghost'}
-      href={path}>
-      {children}
-    </Button>
+      <Button
+          onClick={handleClick}
+          _hover={{ bg: "rgba(34, 139, 230, 0.05)" }}
+          _active={{ bg: "rgba(34, 139, 230, 0.1)" }}
+          rounded={'full'}
+          fontWeight={'normal'}
+          variant={'ghost'}
+      >
+        {children}
+      </Button>
   );
 };
 
 export default function NavBar() {
-
   return (
-    <>
-      <header>
-        <Box className={'nav'} visibility={{base: 'hidden', md: 'visible'}} justifyContent={'center'}>
-          <Center h={20}>
-            <HStack as={'nav'} display={{ base: 'flex', md: 'flex' }}>
-              {navLinks.map(({ name, path }) => (
-                <NavLink key={name} path={path} >{name}</NavLink>
-              ))}
-            </HStack>
-          </Center>
-        </Box>
-      </header>
-    </>
+      <>
+        <header>
+          <Box className={'nav'} visibility={{ base: 'hidden', md: 'visible' }} justifyContent={'center'}>
+            <Center h={20}>
+              <HStack as={'nav'} display={{ base: 'flex', md: 'flex' }}>
+                {navLinks.map(({ name, path }) => (
+                    <NavLink key={name} path={path}>{name}</NavLink>
+                ))}
+              </HStack>
+            </Center>
+          </Box>
+        </header>
+      </>
   );
 }
