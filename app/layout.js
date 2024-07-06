b665d0505aa494c7ed1b2d80c00f55986f6e2441
Ukureii <98332714+Ukureii@users.noreply.github.com>
2024-07-06 18:43:45 +0200

    Skills.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export default function RootLayout({ children }) {
+  return (
+      <html lang="en">
+      <head>
+        <title>Portfolio</title>
+      </head>
+      <body>
+      {children}
+      </body>
+      </html>
+  );
+}
